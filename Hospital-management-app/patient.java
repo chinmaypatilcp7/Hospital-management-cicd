759395e0c1d2fefb068617e2266c2950cabd2577
chinmaypatilcp7 <134280673+chinmaypatilcp7@users.noreply.github.com>
2023-08-04 18:22:24 +0530

    Create patient.java

---
+++ Hospital-management-app/patient.java
@@ -0,0 +1,8 @@
+public class Patient {
+    private int id;
+    private String name;
+    private int age;
+    private String contact;
+
+    // Constructor, getters, setters, and other methods
+}
