299efabcd479b3cd30ca8bc97815cd645580ffc7
chinmaypatilcp7 <134280673+chinmaypatilcp7@users.noreply.github.com>
2023-08-04 18:23:03 +0530

    Create doctor.java

---
+++ Hospital-management-app/doctor.java
@@ -0,0 +1,8 @@
+public class Doctor {
+    private int id;
+    private String name;
+    private String specialization;
+    private String contact;
+
+    // Constructor, getters, setters, and other methods
+}
