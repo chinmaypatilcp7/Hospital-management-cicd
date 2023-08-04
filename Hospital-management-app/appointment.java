d0f1a301c620828cbded9e615ba7f798134d1788
chinmaypatilcp7 <134280673+chinmaypatilcp7@users.noreply.github.com>
2023-08-04 18:23:43 +0530

    Create appointment.java

---
+++ Hospital-management-app/appointment.java
@@ -0,0 +1,9 @@
+public class Appointment {
+    private int id;
+    private int patientId;
+    private int doctorId;
+    private Date appointmentDate;
+    private String reason;
+
+    // Constructor, getters, setters, and other methods
+}
