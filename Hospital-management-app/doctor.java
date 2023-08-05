f8248cae59fe10cd4ed49d2376be4ecd1b3af86b
chinmaypatilcp7 <134280673+chinmaypatilcp7@users.noreply.github.com>
2023-08-05 16:55:44 +0530

    Delete Hospital-management-app directory

--- Hospital-management-app/doctor.java
+++
@@ -1,8 +0,0 @@
-public class Doctor {
-    private int id;
-    private String name;
-    private String specialization;
-    private String contact;
-
-    // Constructor, getters, setters, and other methods
-}
--- Hospital-management-app/patient.java
+++
@@ -1,8 +0,0 @@
-public class Patient {
-    private int id;
-    private String name;
-    private int age;
-    private String contact;
-
-    // Constructor, getters, setters, and other methods
-}
