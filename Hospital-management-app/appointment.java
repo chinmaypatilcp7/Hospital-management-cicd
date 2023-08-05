709c7fc6f21741184fca77fbdf09b53a649abda6
chinmaypatilcp7 <134280673+chinmaypatilcp7@users.noreply.github.com>
2023-08-05 16:04:53 +0530

    Delete Hospital-management-app directory

--- Hospital-management-app/appointment.java
+++
@@ -1,9 +0,0 @@
-public class Appointment {
-    private int id;
-    private int patientId;
-    private int doctorId;
-    private Date appointmentDate;
-    private String reason;
-
-    // Constructor, getters, setters, and other methods
-}
